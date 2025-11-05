cd7e67f29e6e6f4199279338425b7ce01f388df4
jebiyeon02 <jhje5595@naver.com>
2025-11-06 02:14:14 +0900

    feat: 한글-영어 ESLint규칙 Map 생성

---
+++ constants/rule-map.js
@@ -0,0 +1,9 @@
+export const RULE_MAP = {
+  카멜_케이스_검사: "camelcase",
+  메서드_사이_공백: "@stylistic/lines-between-class-members",
+  최대_들여쓰기_깊이: "max-depth",
+  함수_최대_라인수: "max-lines-per-function",
+  삼항연산자_사용: "no-ternary",
+  if문_중괄호_필수: "curly",
+  콘솔_로그_사용: "no-console",
+};
