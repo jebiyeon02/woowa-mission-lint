82180532deea52f87c1bec6906705b0c2cb92bab
jebiyeon02 <jhje5595@naver.com>
2025-11-06 20:09:13 +0900

    feat: 에러 메세지 상수화

---
+++ constants/error-message.js
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGE = {
+  KOREAN_RULE_NAME_NOT_FOUND: "[ERROR]: 일치하는 한글 규칙을 찾지 못했습니다.",
+  PARAMETER_ONLY_CAN_BE_BOOLEAN:
+    "[ERROR]: 파라미터로는 boolean 자료형만 들어올 수 있습니다.",
+};
