fd87829c12827be84e9cd483d367f165fb967c38
jebiyeon02 <jhje5595@naver.com>
2025-11-06 20:31:07 +0900

    test: 옵션 규칙 기능 테스트 추가

--- __tests__/RuleTranslatorTest.js
+++ __tests__/RuleTranslatorTest.js
@@ -1,9 +1,10 @@
 import { ERROR_MESSAGE } from "../constants/error-message.js";
 import { RULE_MAP } from "../constants/rule-map.js";
+import { RULE_STATE } from "../constants/rule-state.js";
 import RuleTranslator from "../src/ruleTranslator.js";
 
 describe("번역기 클래스 테스트", () => {
-  test("번역 기능 테스트", () => {
+  test("규칙 이름 번역 기능 테스트", () => {
     // given
     const koreanRule = "카멜_케이스_검사";
     const englishRule = "camelcase";
@@ -17,7 +18,7 @@ describe("번역기 클래스 테스트", () => {
     );
   });
 
-  test("예외 테스트", () => {
+  test("규칙 이름 번역 예외 테스트", () => {
     // given
     const koreanRule = "없습니다";
 
@@ -29,4 +30,26 @@ describe("번역기 클래스 테스트", () => {
       ERROR_MESSAGE.KOREAN_RULE_NAME_NOT_FOUND
     );
   });
+
+  test.each([
+    [15, [RULE_STATE.TURN_ON_AS_ERROR, 15], false],
+    [true, RULE_STATE.TURN_OFF, true],
+    [false, RULE_STATE.TURN_ON_AS_ERROR, true],
+    [true, RULE_STATE.TURN_ON_AS_ERROR, false],
+    [false, RULE_STATE.TURN_OFF, false],
+    ["off", RULE_STATE.TURN_OFF, false],
+  ])(
+    "옵션 번역 기능 테스트 - woowa 규칙 설정 : %s -> ESLint 규칙 설정 : %s , inverted = %s",
+    (option, result, inverted) => {
+      // when
+      const ruleTranslator = new RuleTranslator(RULE_MAP);
+
+      // then
+      expect(
+        ruleTranslator.translateRuleOptionToEslint(option, inverted)
+      ).toEqual(result);
+    }
+  );
+
+  test("규칙 옵션 번역 기능 테스트", () => {});
 });
