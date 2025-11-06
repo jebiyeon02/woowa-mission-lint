fa60e799471e40793ab67ee94ee42e4aa9f4c365
jebiyeon02 <jhje5595@naver.com>
2025-11-06 20:32:47 +0900

    feat: 규칙 번역 클래스 추가

---
+++ src/RuleTranslator.js
@@ -0,0 +1,54 @@
+import { ERROR_MESSAGE } from "../constants/error-message.js";
+import { RULE_STATE } from "../constants/rule-state.js";
+import TranslatorUtils from "../utils/TranslatorUtils.js";
+
+class RuleTranslator {
+  #ruleNameMap;
+
+  constructor(koreanToEnglishMap) {
+    this.#ruleNameMap = koreanToEnglishMap;
+  }
+
+  translateRuleNameToEnglish(koreanRuleName) {
+    this.#isCorrectKoreanRuleName(koreanRuleName);
+    return this.#ruleNameMap[koreanRuleName];
+  }
+
+  #isCorrectKoreanRuleName(koreanRuleName) {
+    if (!this.#ruleNameMap[koreanRuleName]) {
+      throw new Error(ERROR_MESSAGE.KOREAN_RULE_NAME_NOT_FOUND);
+    }
+  }
+
+  translateRuleOptionToEslint(option, inverted = false) {
+    if (option === "off") {
+      return RULE_STATE.TURN_OFF;
+    }
+
+    // Option이 ESLint와 반대의 Boolean 설정을 가지는 경우
+    if (typeof option === "boolean" && inverted) {
+      const invertedOption = TranslatorUtils.invertBoolean(option);
+      if (invertedOption) {
+        return RULE_STATE.TURN_ON_AS_ERROR;
+      }
+      return RULE_STATE.TURN_OFF;
+    }
+
+    // Option이 ESLint와 같은 Boolean 설정을 가지는 경우
+    if (typeof option === "boolean") {
+      if (option) {
+        return RULE_STATE.TURN_ON_AS_ERROR;
+      }
+      return RULE_STATE.TURN_OFF;
+    }
+
+    // Option이 숫자인 경우
+    if (!isNaN(option)) {
+      return [RULE_STATE.TURN_ON_AS_ERROR, option];
+    }
+
+    return option;
+  }
+}
+
+export default RuleTranslator;
