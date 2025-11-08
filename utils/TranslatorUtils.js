67b1423533fca56e580eac1730ad3947d1b7a5dc
jebiyeon02 <jhje5595@naver.com>
2025-11-08 19:24:06 +0900

    feat: config 파일로부터 특정 레벨의 규칙을 읽는 메서드와 기본 실행 레벨을 읽는 메서드 생성

--- utils/TranslatorUtils.js
+++ utils/TranslatorUtils.js
@@ -1,14 +1,58 @@
-import { ERROR_MESSAGE } from "../constants/error-message";
+import path from "path";
+import fs from "fs";
+import { CONFIG_CONSTANTS } from "../constants/config-constants.js";
+import { ERROR_MESSAGE } from "../constants/error-message.js";
 
 class TranslatorUtils {
-  static invertBoolean(boolean) {
-    if (typeof boolean !== "boolean") {
-      throw new Error(ERROR_MESSAGE.PARAMETER_ONLY_CAN_BE_BOOLEAN);
+  static readKoreanRulesFromConfig(level) {
+    const configPath = TranslatorUtils.#getConfigPath();
+    TranslatorUtils.#validateConfigFile(configPath);
+
+    const configFileContent = JSON.parse(fs.readFileSync(configPath));
+    const allLevelRules =
+      configFileContent[CONFIG_CONSTANTS.OPTION_NAME.EACH_LEVEL_RULE];
+
+    const levelString = `level_${level}`;
+    if (!allLevelRules[levelString]) {
+      throw new Error(ERROR_MESSAGE.CONFIG_LEVEL_NOT_FOUND);
     }
-    if (boolean) {
-      return false;
+    return allLevelRules[levelString];
+  }
+
+  static getDefaultLevelFromConfig() {
+    const configPath = TranslatorUtils.#getConfigPath();
+    TranslatorUtils.#validateConfigFile(configPath);
+
+    const configFileContent = JSON.parse(fs.readFileSync(configPath));
+    const defaultLevelString =
+      configFileContent[CONFIG_CONSTANTS.OPTION_NAME.DEFAULT_RUN_LEVEL];
+    if (!defaultLevelString) {
+      throw new Error(ERROR_MESSAGE.CONFIG_DEFAULT_LEVEL_OPTION_NOT_FOUND);
     }
-    return true;
+    const indexOfUnderBar = defaultLevelString.indexOf(
+      CONFIG_CONSTANTS.DEFAULT_LEVEL_DELIMETER
+    );
+    const defaultLevel = defaultLevelString.substring(indexOfUnderBar + 1);
+
+    return defaultLevel;
+  }
+
+  static #validateConfigFile(configPath) {
+    if (!fs.existsSync(configPath)) {
+      console.error(ERROR_MESSAGE.CONFIG_FILE_NOT_FOUND);
+      process.exit(1);
+    }
+  }
+
+  static #getConfigPath() {
+    const projectRootPath = process.cwd();
+
+    const configPath = path.resolve(
+      projectRootPath,
+      CONFIG_CONSTANTS.CONFIG_FILE_NAME
+    );
+
+    return configPath;
   }
 }
 
