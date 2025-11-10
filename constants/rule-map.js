c0ed163ae51d7806a1d52373cd2b5d0309decafc
jebiyeon02 <jhje5595@naver.com>
2025-11-10 20:53:18 +0900

    feat: SNAKE_CASE 규칙 매핑 추가

--- constants/rule-map.js
+++ constants/rule-map.js
@@ -1,5 +1,6 @@
 export const RULE_MAP = {
   카멜_케이스_검사: 'camelcase',
+  상수_스네이크_케이스_검사: 'woowa/constant-snake-case',
   메서드_사이_공백: '@stylistic/lines-between-class-members',
   최대_들여쓰기_깊이: 'max-depth',
   함수_최대_라인수: 'max-lines-per-function',
