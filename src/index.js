d1c3404701b35b536a47dee98a622be22d512e0a
jebiyeon02 <jhje5595@naver.com>
2025-11-10 21:23:20 +0900

    chore: prettier 적용

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import path from "path";
-import fs from "fs";
-import { CONFIG_CONSTANTS } from "../constants/config-constants.js";
+import path from 'path';
+import fs from 'fs';
+import { CONFIG_CONSTANTS } from '../constants/config-constants.js';
 
 const projectRootPath = process.cwd();
 const configPath = path.resolve(
   projectRootPath,
-  CONFIG_CONSTANTS.CONFIG_FILE_NAME
+  CONFIG_CONSTANTS.CONFIG_FILE_NAME,
 );
 
 let prettierRule;
 
 try {
   const configFileContent = JSON.parse(fs.readFileSync(configPath));
-  prettierRule = configFileContent["프리티어_설정"];
+  prettierRule = configFileContent['프리티어_설정'];
   if (!prettierRule) {
     throw new Error(
-      "[ERROR]: 프리티어_설정이 존재하지 않습니다.\n'프리티어_설정 옵션을 추가 해주세요.'"
+      "[ERROR]: 프리티어_설정이 존재하지 않습니다.\n'프리티어_설정 옵션을 추가 해주세요.'",
     );
   }
 } catch (error) {
-  if (error.code === "ENOENT") {
+  if (error.code === 'ENOENT') {
     console.error(
-      "[ERROR]: woowa-lint.json파일이 존재하지 않습니다.\n'파일을 생성 해주세요.'"
+      "[ERROR]: woowa-lint.json파일이 존재하지 않습니다.\n'파일을 생성 해주세요.'",
     );
     process.exit(1);
   }
