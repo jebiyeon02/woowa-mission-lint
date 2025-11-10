8c2ce68f95b59a40496eb1fc0ffee6f51cd06826
jebiyeon02 <jhje5595@naver.com>
2025-11-11 01:27:53 +0900

    refactor: overrideConfig 생성과 관련된 로직들을 ConfigGenerator 클래스로 분리

---
+++ src/ConfigGenerator.js
@@ -0,0 +1,113 @@
+import { RULE_MAP } from '../constants/rule-map.js';
+import { RULE_STATE } from '../constants/rule-state.js';
+import constantSnakeCaseRule from '../rules/constantSnakeCaseRule.js';
+import RuleTranslator from './ruleTranslator.js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import TranslatorUtils from '../utils/TranslatorUtils.js';
+
+class ConfigGenerator {
+  #koreanRules;
+  #translator;
+
+  constructor(koreanRules) {
+    this.#koreanRules = koreanRules;
+    this.#translator = new RuleTranslator(RULE_MAP);
+  }
+
+  generateConfig() {
+    // makeEslintRules를 실행해도 "적용_폴더" 옵션은 한글 그대로 남아 있음
+    const eslintRules = this.#makeEslintRules(this.#koreanRules);
+    const specificFolders = this.#getSpecificFolders(eslintRules);
+    const { generalRules, specificFolderRules } =
+      this.#separateEslintRules(eslintRules);
+
+    const finalOverrideConfig = [
+      // 전체 파일에 다 적용되는 일반 설정들
+      {
+        files: ['**/*.js'],
+        ignores: specificFolders,
+        languageOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+        plugins: {
+          prettier: eslintPluginPrettier,
+        },
+        rules: {
+          ...generalRules,
+          'prettier/prettier': 'error',
+        },
+      },
+      // 나중에 확장하게 된다면, config를 더 많이 만들어야 할 수도 있다. 이 점 기억해두자
+      eslintConfigPrettier,
+    ];
+    // 특정 폴더 전용 설정들 (특정 폴더 옵션이 있는 경우에만 생성함)
+    if (specificFolders.length > 0) {
+      finalOverrideConfig.unshift({
+        // (prettier-config 앞으로 끼워넣기)
+        files: specificFolders,
+        languageOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+        plugins: {
+          prettier: eslintPluginPrettier,
+          woowa: {
+            rules: {
+              'constant-snake-case': constantSnakeCaseRule,
+            },
+          },
+        },
+        rules: {
+          ...specificFolderRules,
+          camelcase: 'off',
+          'prettier/prettier': 'error',
+        },
+      });
+    }
+
+    return finalOverrideConfig;
+  }
+
+  #makeEslintRules(koreanRules) {
+    const eslintRules = {};
+
+    Object.entries(koreanRules).forEach((koreanRule) => {
+      let { ruleNameEslint, ruleOptionEslint } =
+        this.#translator.translate(koreanRule);
+      if (TranslatorUtils.isPluginRule(ruleNameEslint)) {
+        ruleNameEslint = TranslatorUtils.extractRuleOption(ruleNameEslint);
+      }
+      eslintRules[ruleNameEslint] = ruleOptionEslint;
+    });
+
+    return eslintRules;
+  }
+
+  #separateEslintRules(eslintRules) {
+    // 특정 폴더 제한 규칙이 1개라고 가정한 상황임
+    const SPECIFIC_RULE_NAME = 'woowa/constant-snake-case';
+
+    const specificFolderRules = {};
+    const generalRules = { ...eslintRules };
+
+    // 특정 폴더에만 적용될 규칙과, 모든 곳에 적용될 규칙을 나눔
+    if (eslintRules[SPECIFIC_RULE_NAME]) {
+      // "적용_폴더" 옵션은 사용자 편의성을 위한 config이므로 실제 option값인 "error" (2)로 바꿔줌
+      specificFolderRules[SPECIFIC_RULE_NAME] = RULE_STATE.TURN_ON_AS_ERROR;
+
+      delete generalRules[SPECIFIC_RULE_NAME];
+    }
+
+    return { generalRules, specificFolderRules };
+  }
+
+  #getSpecificFolders(eslintRules) {
+    const specificFolders = [];
+    Object.entries(eslintRules).forEach(([_, value]) => {
+      if (typeof value === 'object' && value['적용_폴더']) {
+        const folderPaths = value['적용_폴더'];
+        specificFolders.push(...folderPaths);
+      }
+    });
+
+    return specificFolders;
+  }
+}
+
+export default ConfigGenerator;
--- src/Translate.js
+++ src/Translate.js
@@ -1,66 +1,32 @@
-import RuleTranslator from './ruleTranslator.js';
-import { RULE_MAP } from '../constants/rule-map.js';
 import { ESLint } from 'eslint';
-import TranslatorUtils from '../utils/TranslatorUtils.js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+import ConfigGenerator from './ConfigGenerator.js';
 
 // 실제로 번역을 실시하는 클래스
+
 class Translate {
   async runLint(koreanRules) {
     console.log('검증을 시작합니다.');
-    const eslintRules = this.#makeEslintRules(koreanRules);
+
+    const configGenerator = new ConfigGenerator(koreanRules);
+    const overrideConfig = configGenerator.generateConfig();
+
     const eslint = new ESLint({
-      // 로컬에 어떤 설정 파일(eslint.config.js 등) 있어도 전부 무시
       overrideConfigFile: true,
-      // 여기서 "플랫 설정"을 직접 넣는다
-      overrideConfig: [
-        {
-          files: ['**/*.js'],
-          languageOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-          // 가상 플러그인으로 커스텀 룰을 주입
-          plugins: {
-            prettier: eslintPluginPrettier,
-            // local: {
-            //   rules: {
-            //     "no-console-log": noConsoleLogRule,
-            //   },
-            // },
-          },
-          rules: {
-            ...eslintRules,
-            'prettier/prettier': 'error',
-          },
-        },
-        eslintConfigPrettier,
-      ],
+      // 여기서 flat 설정을 직접 넣음
+      overrideConfig: overrideConfig,
     });
 
-    // 파일 검사 (또는 lintText 사용 가능)
-    const results = await eslint.lintFiles(['target-code.js']);
+    // 파일 검사
+    const results = await eslint.lintFiles(['.']);
 
+    // 에러 가독성 좋게 출력해주기
     const formatter = await eslint.loadFormatter('stylish');
     console.log(formatter.format(results));
 
     // 에러가 있으면 종료코드 1
     process.exitCode = results.some((r) => r.errorCount > 0) ? 1 : 0;
     console.log('검증 완료!');
-  }
-
-  #makeEslintRules(koreanRules) {
-    const translator = new RuleTranslator(RULE_MAP);
-    const englishRules = {};
-
-    Object.entries(koreanRules).forEach((koreanRule) => {
-      let { ruleNameEslint, ruleOptionEslint } =
-        translator.translate(koreanRule);
-      if (TranslatorUtils.isPluginRule(ruleNameEslint)) {
-        ruleNameEslint = TranslatorUtils.extractRuleOption(ruleNameEslint);
-      }
-      englishRules[ruleNameEslint] = ruleOptionEslint;
-    });
-
-    return englishRules;
   }
 }
 
