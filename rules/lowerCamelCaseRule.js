09859430c429a6340433bf1c7baa37520873e49e
jebiyeon02 <jhje5595@naver.com>
2025-11-12 00:11:53 +0900

    feat: 변수 또는 함수명이 영어 소문자와 calmeCase로 이루어져있는지 체크하는 커스텀 규칙 생성

---
+++ rules/lowerCamelCaseRule.js
@@ -0,0 +1,35 @@
+import { CONFIG_CONSTANTS } from '../constants/config-constants.js';
+
+const lowerCamelCaseRule = {
+  meta: {
+    type: 'problem',
+    messages: {
+      notCamelCase:
+        '변수 또는 함수명은 카멜케이스로 작성해야 합니다 (잘못된 이름: {{name}})',
+    },
+    schema: [],
+  },
+
+  create(context) {
+    return {
+      VariableDeclarator(node) {
+        if (node.id.type !== 'Identifier') return;
+
+        const name = node.id.name;
+
+        const regex = new RegExp(CONFIG_CONSTANTS.LOWER_CAMEL_CASE_REGEX);
+
+        if (!regex.test(name)) {
+          context.report({
+            node,
+            messageId: 'notCamelCase',
+            data: {
+              name: name,
+            },
+          });
+        }
+      },
+    };
+  },
+};
+export default lowerCamelCaseRule;
