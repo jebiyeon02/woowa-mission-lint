ec471cdf720cce82316e0f3527874c7748347081
jebiyeon02 <jhje5595@naver.com>
2025-11-12 00:10:56 +0900

    feat: 변수가 포함된 에러 메세지를 번역하는 유틸 추가

--- utils/TranslatorUtils.js
+++ utils/TranslatorUtils.js
@@ -21,6 +21,66 @@ class TranslatorUtils {
     return false;
   }
 
+  // 변수들이 존재하는 원본 에러 메세지로부터 변수들을 추출하는 유틸 -> 한글로 변환할 때 그대로 사용하기 위함
+  static extractVariablesFromMessage(message) {
+    if (message.ruleId === 'max-lines-per-function') {
+      const variables = [];
+
+      // 화살표 함수 확인
+      if (
+        String(message.message).includes('Arrow function') ||
+        String(message.message).includes('arrow function')
+      ) {
+        variables.push('화살표 함수');
+      }
+
+      // private 함수 확인
+      const foundPrivateMethod = String(message.message).match(/#\S+/g);
+      if (foundPrivateMethod) {
+        variables.push(`private 메서드 ${foundPrivateMethod[0]}`);
+      }
+
+      // 일반 함수 또는 public 메서드
+      const foundFunctionName = String(message.message).match(/'(.*?)'/g);
+      if (foundFunctionName) {
+        variables.push(foundFunctionName[0]);
+      }
+
+      const foundNumbers = String(message.message).match(/\d+/g);
+      if (foundNumbers) {
+        variables.push(...foundNumbers);
+      }
+
+      return variables;
+    }
+
+    if (message.ruleId === 'max-depth') {
+      const variables = [];
+
+      const foundNumbers = String(message.message).match(/\d+/g);
+      if (foundNumbers) {
+        variables.push(...foundNumbers);
+      }
+
+      return variables;
+    }
+  }
+
+  static replaceMessageWithArray(message, targetString, array) {
+    if (!message.includes(targetString) || array.length === 0) {
+      return message;
+    }
+
+    const variable = array.shift();
+    const newMessage = message.replace(targetString, variable);
+
+    return TranslatorUtils.replaceMessageWithArray(
+      newMessage,
+      targetString,
+      array,
+    );
+  }
+
   static readKoreanRulesFromConfig(level) {
     const configPath = TranslatorUtils.#getConfigPath();
     TranslatorUtils.#validateConfigFile(configPath);
