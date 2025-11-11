9b7634e9cba581f7f632796cd84fc9cf9e3501f5
jebiyeon02 <jhje5595@naver.com>
2025-11-11 22:20:41 +0900

    feat: ESLint 내부 카멜 케이스 규칙에서 커스텀 카멜 케이스 규칙으로 변경

--- constants/rule-map.js
+++ constants/rule-map.js
@@ -1,5 +1,5 @@
 export const RULE_MAP = {
-  카멜_케이스_검사: 'camelcase',
+  카멜_케이스_검사: 'woowa/camelCase',
   상수_스네이크_케이스_검사: 'woowa/constant-snake-case',
   메서드_사이_공백: '@stylistic/lines-between-class-members',
   최대_들여쓰기_깊이: 'max-depth',
