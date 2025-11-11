d180150f52563414f61da08756e47762efa03477
jebiyeon02 <jhje5595@naver.com>
2025-11-11 17:01:17 +0900

    feat: prettier 한글설정과 영어설정을 일대일로 대응

---
+++ constants/prettier-map.js
@@ -0,0 +1,9 @@
+export const PRETTIER_MAP = {
+  홑따옴표_사용: 'singleQuote',
+  세미콜론_사용: 'semi',
+  탭_사용: 'useTabs',
+  탭_너비: 'tabWidth',
+  한_줄당_최대_글자수: 'printWidth',
+  화살표_함수_괄호: 'arrowParens',
+  후행_콤마: 'trailingComma',
+};
