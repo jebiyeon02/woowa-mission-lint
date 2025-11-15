27bae6e4f5640eac60be9f9be093d600842804b7
jebiyeon02 <jhje5595@naver.com>
2025-11-15 23:31:06 +0900

    refactor: 에러 태그 제외

--- constants/error-message.js
+++ constants/error-message.js
@@ -1,13 +1,15 @@
 import { CONFIG_CONSTANTS } from './config-constants.js';
 
 export const ERROR_MESSAGE = {
-  KOREAN_RULE_NAME_NOT_FOUND: '[ERROR]: 일치하는 한글 규칙을 찾지 못했습니다.',
+  KOREAN_RULE_NAME_NOT_FOUND: '일치하는 한글 규칙을 찾지 못했습니다.',
   PARAMETER_ONLY_CAN_BE_BOOLEAN:
-    '[ERROR]: 파라미터로는 boolean 자료형만 들어올 수 있습니다.',
+    '파라미터로는 boolean 자료형만 들어올 수 있습니다.',
 
-  CONFIG_FILE_NOT_FOUND: `[ERROR]: 설정 파일(${CONFIG_CONSTANTS.CONFIG_FILE_NAME})을 찾을 수 없습니다.
-  \n${CONFIG_CONSTANTS.INIT_COMMAND} 명령을 실행하여 설정 파일을 생성하세요.`,
+  ESLINT_RULE_ID_NOT_FOUND: '일치하는 옵션이 존재하지 않습니다.',
+  CONFIG_OPTION_NOT_FOUND: '해당 설정을 찾을 수 없습니다.',
 
-  INCLUDE_BLANK: '[ERROR]: 규칙에 공백이 포함되어 있습니다.',
-  RULE_NOT_FOUND: '[ERROR]: 일치하는 규칙을 찾지 못했습니다.',
+  CONFIG_FILE_NOT_FOUND: `설정 파일(${CONFIG_CONSTANTS.CONFIG_FILE_NAME})을 찾을 수 없습니다.\n${CONFIG_CONSTANTS.INIT_COMMAND} 명령을 실행하여 설정 파일을 생성하세요.`,
+
+  INCLUDE_BLANK: '규칙에 공백이 포함되어 있습니다.',
+  RULE_NOT_FOUND: '일치하는 규칙을 찾지 못했습니다.',
 };
