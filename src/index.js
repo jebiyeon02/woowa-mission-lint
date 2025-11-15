cedd926d1035a0469adc5e97435c4af8381b62aa
jebiyeon02 <jhje5595@naver.com>
2025-11-15 23:41:51 +0900

    refactor: index.cjs와 완전히 동일하게 작동하도록 변경

--- src/index.js
+++ src/index.js
@@ -1,36 +1,67 @@
 import path from 'path';
 import fs from 'fs';
+import { fileURLToPath } from 'url';
 import { CONFIG_CONSTANTS } from '../constants/config-constants.js';
 import PrettierTranslator from './PrettierTranslator.js';
 import { PRETTIER_MAP } from '../constants/prettier-map.js';
 
-const projectRootPath = process.cwd();
-const configPath = path.resolve(
-  projectRootPath,
-  CONFIG_CONSTANTS.CONFIG_FILE_NAME,
-);
+/*
+  prettier는 index.cjs만 사용하지만, 특정 환경에서 index.js를 사용할 가능성을 대비해
+  index.js에도 index.cjs와 같은 로직을 구현해놓음
+ */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-let prettierRule;
+function findConfigPath() {
+  let currentDir = __dirname;
+  const systemRoot = path.parse(currentDir).root;
 
-try {
-  const configFileContent = JSON.parse(fs.readFileSync(configPath));
-  prettierRule = configFileContent['프리티어_설정'];
-  if (!prettierRule) {
-    throw new Error(
-      "[ERROR]: 프리티어_설정이 존재하지 않습니다.\n'프리티어_설정 옵션을 추가 해주세요.'",
-    );
+  while (currentDir !== systemRoot) {
+    const projectMarker = path.resolve(currentDir, 'package.json');
+
+    if (fs.existsSync(projectMarker)) {
+      const woowaConfigPath = path.resolve(
+        currentDir,
+        CONFIG_CONSTANTS.CONFIG_FILE_NAME,
+      );
+      if (fs.existsSync(woowaConfigPath)) {
+        return woowaConfigPath;
+      }
+    }
+
+    currentDir = path.dirname(currentDir);
   }
-} catch (error) {
-  if (error.code === 'ENOENT') {
-    console.error(
-      "[ERROR]: woowa-lint.json파일이 존재하지 않습니다.\n'파일을 생성 해주세요.'",
-    );
-    process.exit(1);
-  }
-  console.error(error.message);
+  return null;
 }
 
-const prettierTranslator = new PrettierTranslator(PRETTIER_MAP);
-prettierRule = prettierTranslator.translateAllRules(prettierRule);
+function loadUserConfig() {
+  const configPath = findConfigPath();
+  let prettierRule;
 
-export default prettierRule;
+  try {
+    if (configPath) {
+      const configFileContent = JSON.parse(
+        fs.readFileSync(configPath, 'utf-8'),
+      );
+      prettierRule = configFileContent['프리티어_설정'];
+
+      if (!prettierRule) {
+        console.warn('[ERROR]: "프리티어_설정" 키가 없습니다.');
+        return {};
+      }
+    } else {
+      console.warn('[ERROR]: woowalint.json 파일을 찾을 수 없습니다.');
+      return {};
+    }
+  } catch (error) {
+    console.error('[ERROR]: woowalint.json 처리 중 오류:', error.message);
+    return {};
+  }
+
+  const prettierTranslator = new PrettierTranslator(PRETTIER_MAP);
+  const finalPrettierRule = prettierTranslator.translateAllRules(prettierRule);
+
+  return finalPrettierRule;
+}
+
+export default loadUserConfig();
