5cef2afaf9ecac5a7b97e8157de82f2f2d6f9adb
jebiyeon02 <jhje5595@naver.com>
2025-11-15 21:41:28 +0900

    refactor: 파일 읽기와 관련된 메서드들을 삭제, addLevelPrefix 메서드 생성

--- utils/TranslatorUtils.js
+++ utils/TranslatorUtils.js
@@ -1,9 +1,8 @@
-import path from 'path';
-import fs from 'fs';
-import { CONFIG_CONSTANTS } from '../constants/config-constants.js';
-import { ERROR_MESSAGE } from '../constants/error-message.js';
-
 class TranslatorUtils {
+  static addLevelPrefix(level) {
+    return `level_${level}`;
+  }
+
   static substringAfterChar(string, char) {
     const indexOfChar = string.indexOf(char);
 
@@ -79,57 +78,6 @@ class TranslatorUtils {
       targetString,
       array,
     );
-  }
-
-  static readKoreanRulesFromConfig(level) {
-    const configPath = TranslatorUtils.#getConfigPath();
-    TranslatorUtils.#validateConfigFile(configPath);
-
-    const configFileContent = JSON.parse(fs.readFileSync(configPath));
-    const allLevelRules =
-      configFileContent[CONFIG_CONSTANTS.OPTION_NAME.EACH_LEVEL_RULE];
-
-    const levelString = `level_${level}`;
-    if (!allLevelRules[levelString]) {
-      throw new Error(ERROR_MESSAGE.CONFIG_LEVEL_NOT_FOUND);
-    }
-    return allLevelRules[levelString];
-  }
-
-  static getDefaultLevelFromConfig() {
-    const configPath = TranslatorUtils.#getConfigPath();
-    TranslatorUtils.#validateConfigFile(configPath);
-
-    const configFileContent = JSON.parse(fs.readFileSync(configPath));
-    const defaultLevelString =
-      configFileContent[CONFIG_CONSTANTS.OPTION_NAME.DEFAULT_RUN_LEVEL];
-    if (!defaultLevelString) {
-      throw new Error(ERROR_MESSAGE.CONFIG_DEFAULT_LEVEL_OPTION_NOT_FOUND);
-    }
-    const defaultLevel = TranslatorUtils.substringAfterChar(
-      defaultLevelString,
-      CONFIG_CONSTANTS.DEFAULT_LEVEL_DELIMETER,
-    );
-
-    return defaultLevel;
-  }
-
-  static #validateConfigFile(configPath) {
-    if (!fs.existsSync(configPath)) {
-      console.error(ERROR_MESSAGE.CONFIG_FILE_NOT_FOUND);
-      process.exit(1);
-    }
-  }
-
-  static #getConfigPath() {
-    const projectRootPath = process.cwd();
-
-    const configPath = path.resolve(
-      projectRootPath,
-      CONFIG_CONSTANTS.CONFIG_FILE_NAME,
-    );
-
-    return configPath;
   }
 }
 
