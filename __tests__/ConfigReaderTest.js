794066742e63a70fae63f8b31eb2de953c4891b2
jebiyeon02 <jhje5595@naver.com>
2025-11-24 02:04:36 +0900

    test: spyOn을 사용해 클래스를 테스트

--- __tests__/ConfigReaderTest.js
+++ __tests__/ConfigReaderTest.js
@@ -1,27 +1,50 @@
+import fs from 'fs';
 import ConfigReader from '../src/ConfigReader.js';
+import { jest } from '@jest/globals';
 
-describe('설정 읽기 클래스 테스트', () => {
-  test.each([
-    ['root', '기본_실행_레벨', 'level_1'],
-    ['root', '검사_제외_폴더', ['__tests__/**', 'node_modules/**']],
-  ])('기능 테스트', (parent, option, result) => {
-    // when
-    const configReader = new ConfigReader();
+describe('ConfigReader 클래스 테스트', () => {
+  const testConfigContent = {
+    기본_실행_레벨: 'level_1',
+    검사_제외_폴더: ['__tests__/**', 'node_modules/**'],
+  };
 
-    // then
-    expect(configReader.getOptionContents(parent, option)).toEqual(result);
+  let readFileSyncSpy;
+
+  beforeEach(() => {
+    readFileSyncSpy = jest
+      .spyOn(fs, 'readFileSync')
+      .mockReturnValue(JSON.stringify(testConfigContent));
   });
 
-  test('예외 테스트', () => {
-    // given
-    const mockOption = '우테코';
+  // 각 테스트가 끝난 후 모든 모킹을 복원함
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
 
-    // when
+  test('설정 파일의 내용을 올바르게 읽고 값을 반환해야 한다', () => {
+    // given
     const configReader = new ConfigReader();
 
-    // then
-    expect(() => configReader.getOptionContents('root', mockOption)).toThrow(
-      '[ERROR]',
+    // when
+    const level = configReader.getOptionContents('root', '기본_실행_레벨');
+    const exceptFolders = configReader.getOptionContents(
+      'root',
+      '검사_제외_폴더',
     );
+
+    // then
+    expect(readFileSyncSpy).toHaveBeenCalledTimes(1);
+    expect(level).toBe('level_1');
+    expect(exceptFolders).toEqual(['__tests__/**', 'node_modules/**']);
+  });
+
+  test('존재하지 않는 옵션을 읽으려고 할 때 에러를 발생시킨다', () => {
+    // given
+    const configReader = new ConfigReader();
+
+    // when & then
+    expect(() => {
+      configReader.getOptionContents('root', '없는_옵션');
+    }).toThrow();
   });
 });
